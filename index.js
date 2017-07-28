8dc2b9fc0a47f1483d986df5ddc5275b59840f70
alexwalling <alex.walling19@gmail.com>
2017-07-28 15:11:48 -0600

    adding employee name tracking

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const RapidAPI = require('rapidapi-connect');
-const rapid = new RapidAPI("default-application_5947e9b9e4b023d4b55e2d4b", "d2494848-1781-4a42-b9d3-c378074cc993");
+const rapid = new RapidAPI("", "");
 var https = require('https');
 var fs = require('fs');
 
 var cursor;
+var employee_names = [];
 
-var token = '3KdYdieRFCUAAAAAAAAEswylPWtNCejAgaLdy6-d4OKqwbdOZXeCgjO0C2zWHoyH';
+var token = '';
+
+
+/*
+	Dropbox file handling
+*/
 
 /*
 	INIT CURSOR
@@ -15,26 +21,16 @@ rapid.call('Dropbox', 'getFolderContents', {
 	'folderPath': '/rapid-api-upload'
 }).on('success', (payload)=>{
 	cursor = payload[0].cursor;
-	console.log(cursor);
-	//console.log(payload[0].entries);
 }).on('error', (payload)=>{
 	console.log(payload);
 });
 
-
-
-
 function downloadFile(path_to_file){
-	const RapidAPI = require('rapidapi-connect');
-	const rapid = new RapidAPI("default-application_5947e9b9e4b023d4b55e2d4b", "d2494848-1781-4a42-b9d3-c378074cc993");
-
-	console.log(path_to_file);
 	rapid.call('Dropbox', 'downloadFile', { 
 		'accessToken': token,
 		//need to use path_to_file instead
 		'filePath': path_to_file
 	}).on('success', (payload)=>{
-		console.log(payload);
 		//make the file based on the filename passed in
 		var filename = path_to_file.replace(/^.*[\\\/]/, '');
 		console.log(filename);
@@ -56,12 +52,20 @@ function checkForUpload(){
 		'accessToken': token,
 		'cursor': cursor
 	}).on('success', (payload)=>{
-		console.log('SUCCESS');
+		console.log(payload);
+		console.log(payload[0].entries);
 		files = payload[0].entries;
 		if(files.length > 0){
 			for(var i = 0; i < files.length; i++)
 			if(files[i]['.tag'] == 'file'){
-				downloadFile(files[i].path_lower);
+				path = files[i].path_lower
+				filename = path.replace(/^.*[\\\/]/, '');
+				ind = filename.indexOf('-');
+				name = filename.substring(0, ind);
+				if(!employee_names.includes(name)){
+					employee_names.push(name);
+				}
+				console.log(names);
 			}
 		}
 		
@@ -104,3 +108,7 @@ rapid.listen('Dropbox', 'webhookEvent', {
 .on('close', (reason) => {
 
 });
+
+
+
+
